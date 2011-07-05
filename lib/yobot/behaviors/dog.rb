6ac0aa969356507d936aeba2fc4e20cb09487d07
Joan Wolkerstorfer <joanwolk@gmail.com>
2011-07-05 15:05:38 +0200

    Add comment regarding Cheezburger API deprecation

--- lib/yobot/behaviors/dog.rb
+++ lib/yobot/behaviors/dog.rb
@@ -4,6 +4,7 @@ class Yobot::Behaviors::Dog
 	def react(room, message)
     if message =~ /^dog/
       request = EventMachine::HttpRequest.new('http://api.cheezburger.com/xml/category/dogs/lol/random')
+			# This version of the Cheezburger API is deprecated as of July 2011 and may break when a new API is released
       http = request.get
       http.callback do
         room.paste(extract_image_url(http.response)) {}
